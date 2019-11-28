5454f5c4253c6dc636f03aa82566e33b13ed66d6
Leonardo de-Oliveira-Martins (QIB) <Leonardo.de-Oliveira-Martins@quadram.ac.uk>
2019-11-28 15:26:27 +0000

    layout

--- docs/site_libs/tipuesearch/tipuesearch_content.js
+++ docs/site_libs/tipuesearch/tipuesearch_content.js
@@ -6,6 +6,6 @@ var tipuesearch = {"pages": [
 {"mainTitle": "License", "title": "Website layout", "text": "Website layout ", "tags": "", "mainUrl": "http://leomrtns.github.io/notebooks/License.html", "url": "http://leomrtns.github.io/notebooks/License.html#Website-layout"},
 {"mainTitle": "Bioinformatics notebooks", "title": "Bioinformatics notebooks", "text": "Bioinformatics notebooks ", "tags": "", "mainUrl": "http://leomrtns.github.io/notebooks/index.html", "url": "http://leomrtns.github.io/notebooks/index.html#Bioinformatics-notebooks"},
 {"mainTitle": "Bioinformatics notebooks", "title": "Project overview", "text": "Project overview ", "tags": "", "mainUrl": "http://leomrtns.github.io/notebooks/index.html", "url": "http://leomrtns.github.io/notebooks/index.html#Project-overview"},
-{"mainTitle": "first C code", "title": "first C code", "text": "first C code ", "tags": "", "mainUrl": "http://leomrtns.github.io/notebooks/Coding/first.html", "url": "http://leomrtns.github.io/notebooks/Coding/first.html#first-C-code"},
-{"mainTitle": "Coding", "title": "Coding", "text": "Coding ", "tags": "", "mainUrl": "http://leomrtns.github.io/notebooks/Coding/second.html", "url": "http://leomrtns.github.io/notebooks/Coding/second.html#Coding"}
+{"mainTitle": "002 first C code", "title": "002 first C code", "text": "002 first C code ", "tags": "", "mainUrl": "http://leomrtns.github.io/notebooks/Coding/first.html", "url": "http://leomrtns.github.io/notebooks/Coding/first.html#002-first-C-code"},
+{"mainTitle": "001 try", "title": "001 try", "text": "001 try ", "tags": "", "mainUrl": "http://leomrtns.github.io/notebooks/Coding/second.html", "url": "http://leomrtns.github.io/notebooks/Coding/second.html#001-try"}
 ]};
